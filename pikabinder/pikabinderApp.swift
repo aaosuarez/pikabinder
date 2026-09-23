c430cddde46d88ed2141b26af884471aa1a8917d
Aaron Suarez <aaosuarez@gmail.com>
2026-09-23 08:17:08 -0700

    Initial Commit

---
+++ pikabinder/pikabinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  pikabinderApp.swift
+//  pikabinder
+//
+//  Created by Aaron Suarez on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct pikabinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pikabinderTests/pikabinderTests.swift
@@ -0,0 +1,19 @@
+//
+//  pikabinderTests.swift
+//  pikabinderTests
+//
+//  Created by Aaron Suarez on 9/23/26.
+//
+
+import Testing
+@testable import pikabinder
+
+struct pikabinderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}

dabfecd26ec5774996f237038f752c99fb243b6f
Aaron Suarez <aaosuarez@gmail.com>
2026-09-25 19:48:53 -0700

    Remove print statement

--- pikabinder/Views/ContentView.swift
+++ pikabinder/Views/ContentView.swift
@@ -19,7 +19,6 @@ struct ContentView: View {
     @State private var selectedCard: Card? = nil
 
     var sortedCards: [Card] {
-        print(cards.count)
         return cards.sorted { a, b in
             let isAOwned = ownedCardIds.contains(a.id)
             let isBOwned = ownedCardIds.contains(b.id)
